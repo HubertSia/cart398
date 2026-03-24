3aea4176df3ca45728a2ebdbebca0ee9bae13c0a
HubertSia <hubemail2021@gmail.com>
2026-03-24 14:39:03 -0400

    update style

--- strudel-pie-main/patch.js
+++ strudel-pie-main/patch.js
@@ -10,26 +10,37 @@ let melMuted = false;
 let currentCps = 0.03;
 
 const buildPatch = () => `
-  setcps(${currentCps})
-  stack(
-    sound("808bd:6*16")
-      .speed(saw.range(1,5).slow(1))
-      .every(8, x=>x.hurry(2).gain(0.125).slow(2))
-      .gain(${bdMuted ? 0 : 0.5}),
-    // sound("[~ 808sd:2]!2")
-    sound("[~ ~ ~ pebbles?]")
-      .speed(0.5)
-      .pan(sine)
-      .gain(${bdMuted ? 0 : 0.125}),
-    note("[c d?0.8 e?0.8 f?0.8 g?0.8 a?0.8 b?0.8 c4?0.8]*4")
-      .add(-12)
-      .sound('newnotes')
-      .jux(rev)
-      .room(0.25)
-      .delay(0.25)
-      .delayt(0.125)
-      .gain(${melMuted ? 0 : 0.125})
-  ).play()
+  setcps(130/60/5)
+
+
+  $: note("<[d3 d3 c3 c3] [f3 f3 g2]  >*2 <[c3 c2 g3 f3] [d3 d3 e3 f3] >*2 ")
+    .sound("gm_lead_3_calliope")
+    .scale("e:major")
+    .gain(0.5)
+    .firstOf(4, x=>x.rev())
+
+$: note("<[g3 f3 e3 d3] [f3 e3 d3 c3] f3 d3 e3 >*2 < a3 g3 f3 e3 [d3 e3 f3 g3] [c3 d3 e3 f3] d3 e3 f3 >*2")
+  .sound("gm_electric_guitar_clean")
+  .gain(0.5)
+  .firstOf(4, x=>x.rev())
+
+
+$: n("< 1 3 3 1 2 2 5>*8")
+  .scale("e:major")
+  .s("gm_piano")
+  .gain(0.5)
+  .fast(2)
+  .lpf(300)
+  .gain(0.7)
+  .lpenv("<4 3 2>*4")
+  .firstOf(5, x=>x.rev())  
+
+
+  $: s("cp").struct("{1 0 0 1 0 1 0 0}%8")
+    .gain(0.5)
+    .firstOf(10, x=>x.rev())
+
+  .play()
   `;
 
 const playPattern = async () => {
